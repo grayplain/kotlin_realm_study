2efed719a5bf60922fd20b0893e6fedb0a4f4d93
joge <you@example.com>
2018-07-19 22:43:39 +0100

    現時点で使う予定のないメソッドを除却した。

--- app/src/main/java/avenue/joke/relation/buisiness/soot/com/jokeavenue/JokeListFragment.kt
+++ app/src/main/java/avenue/joke/relation/buisiness/soot/com/jokeavenue/JokeListFragment.kt
@@ -15,17 +15,13 @@ import avenue.joke.relation.buisiness.soot.com.jokeavenue.dummy.DummyContent.Dum
 
 class JokeListFragment : Fragment() {
 
-    // TODO: Customize parameters
     private var columnCount = 1
 
     private var listener: OnListFragmentInteractionListener? = null
 
+    //onCreateView の前に呼ばれるらしいが・・・。 onCreateView だけでいいんじゃないか？
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -47,18 +43,6 @@ class JokeListFragment : Fragment() {
         return view
     }
 
-    // トップのアクティビティに情報を渡す予定はないんで、コメントアウト
-    /*
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
-    */
-
     override fun onDetach() {
         super.onDetach()
         listener = null
@@ -67,20 +51,5 @@ class JokeListFragment : Fragment() {
     interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onListFragmentInteraction(item: DummyItem?)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-                JokeListFragment().apply {
-                    arguments = Bundle().apply {
-                        putInt(ARG_COLUMN_COUNT, columnCount)
-                    }
-                }
     }
 }
--- app/src/main/java/avenue/joke/relation/buisiness/soot/com/jokeavenue/TopMenuActivity.kt
+++ app/src/main/java/avenue/joke/relation/buisiness/soot/com/jokeavenue/TopMenuActivity.kt
@@ -2,15 +2,10 @@ package avenue.joke.relation.buisiness.soot.com.jokeavenue
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import avenue.joke.relation.buisiness.soot.com.jokeavenue.JokeAvenueRealm.JAJokeObject
-import io.realm.Realm
-import io.realm.RealmConfiguration
-import java.util.*
 
 class TopMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_top_test)
+        setContentView(R.layout.activity_top_fragment)
     }
 }
